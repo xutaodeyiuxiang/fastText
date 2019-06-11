4de4bd428283d2062bdd1ada1313fce6f332a263
xutaodeyiuxiang <xutaodeyiuxiang@126.com>
2019-06-11 17:26:35 +0800

    productquantizer.h
    productquantizer.h comment

--- src/productquantizer.h
+++ src/productquantizer.h
@@ -6,6 +6,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+/*
+ 这个文件的目的是 压缩一个matrix，对应的是embedding矩阵。采用的算法是product quantizer, 最早用在图像检索引擎中
+ 假设embedding 有 512维
+ （1）训练编码本：将512维度划分成16份，每份32维。对每份数据进行kmeans聚类，类别数量是256，最终我们得到 16 * 256个聚类中心
+ （2）量化 ： 将512维度划分成16份，每份32维。每份找到最近的聚类中心，将聚类中心的id，存储下来，最后会有16个聚类中心id，表示原有的向量，就压缩了
+ */
+
 #pragma once
 
 #include <cstring>
@@ -21,20 +28,20 @@ namespace fasttext {
 
 class ProductQuantizer {
  protected:
-  const int32_t nbits_ = 8;
-  const int32_t ksub_ = 1 << nbits_;
+  const int32_t nbits_ = 8;                                    
+  const int32_t ksub_ = 1 << nbits_;                            // 每份kmeans聚类中心的个数 2^nbits_ = 2^8 = 256
   const int32_t max_points_per_cluster_ = 256;
   const int32_t max_points_ = max_points_per_cluster_ * ksub_;
-  const int32_t seed_ = 1234;
-  const int32_t niter_ = 25;
-  const real eps_ = 1e-7;
+  const int32_t seed_ = 1234;                                   // kmenas初始化随机种子
+  const int32_t niter_ = 25;                                    // kmeans迭代次数
+  const real eps_ = 1e-7;                                       // kmeans训练的eps
 
-  int32_t dim_;
-  int32_t nsubq_;
-  int32_t dsub_;
-  int32_t lastdsub_;
+  int32_t dim_;                                                 // 待压缩的embedding的维度
+  int32_t nsubq_;                                               // 划分成多少个子空间
+  int32_t dsub_;                                                // 每个空间的维度  
+  int32_t lastdsub_;                                            // 最后一个子空间的维度 dsub_ * nsubq_ + lastdsub_ = dim_
 
-  std::vector<real> centroids_;
+  std::vector<real> centroids_;                                 // 聚类中心数据存放
 
   std::minstd_rand rng;
 
