d6e0a6afe3337769e66d08675d31ef7606cecd1f
xutaodeyiuxiang <xutaodeyiuxiang@126.com>
2019-06-12 12:11:09 +0800

    model.cc
    model.cc

--- src/model.cc
+++ src/model.cc
@@ -16,6 +16,7 @@
 
 namespace fasttext {
 
+// 只是用来存储此批的中间结果，不会涉及到具体的参数，更新的参数都是在class Model里面
 Model::State::State(int32_t hiddenSize, int32_t outputSize, int32_t seed)
     : lossValue_(0.0),
       nexamples_(0),
@@ -24,10 +25,12 @@ Model::State::State(int32_t hiddenSize, int32_t outputSize, int32_t seed)
       grad(hiddenSize),
       rng(seed) {}
 
+// 此批数据的平均loss
 real Model::State::getLoss() const {
-  return lossValue_ / nexamples_;
+  return lossValue_ / nexamples_; 
 }
 
+// 每个例子增加一个loss
 void Model::State::incrementNExamples(real loss) {
   lossValue_ += loss;
   nexamples_++;
@@ -44,10 +47,11 @@ void Model::computeHidden(const std::vector<int32_t>& input, State& state)
     const {
   Vector& hidden = state.hidden;
   hidden.zero();
+  // hidden 向量保存输入词向量的均值, 也会存储到state里面
   for (auto it = input.cbegin(); it != input.cend(); ++it) {
-    hidden.addRow(*wi_, *it);
+    hidden.addRow(*wi_, *it);    // addRow 的作用是将 wi_ 矩阵的第 *it 列加到 hidden 上 
   }
-  hidden.mul(1.0 / input.size());
+  hidden.mul(1.0 / input.size()); // 求和后除以输入词个数，得到均值向量
 }
 
 void Model::predict(
@@ -67,6 +71,7 @@ void Model::predict(
   loss_->predict(k, threshold, heap, state);
 }
 
+// 一个sample的更新过程
 void Model::update(
     const std::vector<int32_t>& input,
     const std::vector<int32_t>& targets,
@@ -76,21 +81,26 @@ void Model::update(
   if (input.size() == 0) {
     return;
   }
+  // 计算前向传播：输入层 -> 隐层（做完average之后的隐层）
   computeHidden(input, state);
 
   Vector& grad = state.grad;
   grad.zero();
+  // 不仅通过前向传播算出了 loss_，还进行了反向传播，计算出了 grad_
   real lossValue = loss_->forward(targets, targetIndex, state, lr, true);
-  state.incrementNExamples(lossValue);
+  state.incrementNExamples(lossValue); 
 
   if (normalizeGradient_) {
+    // 将 grad_ 除以 input_ 的大小
     grad.mul(1.0 / input.size());
   }
+  // 反向传播，将 hidden_ 上的梯度传播到 wi_ 上的对应行
   for (auto it = input.cbegin(); it != input.cend(); ++it) {
     wi_->addVectorToRow(grad, *it, 1.0);
   }
 }
 
+// 防止x为0的情况
 real Model::std_log(real x) const {
   return std::log(x + 1e-5);
 }
